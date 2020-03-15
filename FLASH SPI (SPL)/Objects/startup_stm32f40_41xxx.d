73a1b9cd858e2a57b4458ace40336f702f97181d
Vladyslav Daliavskyi <mobivlad@outlook.com>
2020-03-15 21:00:48 +0200

    Removed gitignore files

--- FLASH SPI (SPL)/Objects/startup_stm32f40_41xxx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f40_41xxx.o: cmsis\src\startup_stm32f40_41xxx.s
--- UART Send Data (CMSIS-Driver)/Objects/main.d
+++
@@ -1,8 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: C:\Program Files\Keil_v5\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_USART.h
-.\objects\main.o: C:\Program Files\Keil_v5\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_Common.h
-.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
